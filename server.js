e82ae1f88d9904aad951e89efea9cded7bc1756a
rabeb-bb <rabeb.bahry@gmail.com>
2021-11-21 12:33:53 +0100

    version 1.0

--- server.js
+++ server.js
@@ -5,19 +5,6 @@ const multer = require("multer");
 const path = require("path");
 // ***************
 const app = express();
-if (process.env.NODE_ENV == "production") {
-  app.use(express.static(path.join(__dirname, "client", "build")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
-} else {
-  require("dotenv").config();
-}
-// ***************
-// connect to the DB
-connectDB();
-//****************
 
 //router
 app.use(express.json());
@@ -31,7 +18,21 @@ app.use("/api/support", require("./router/support"));
 // ***********
 PORT = process.env.PORT;
 // ***********
+
 console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+} else {
+  require("dotenv").config();
+}
+// ***************
+// connect to the DB
+connectDB();
+//****************
 
 //************
 app.listen(PORT, (err) => {
